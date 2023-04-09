eaf2438db40c3a91ecae2771273daf05b953fad6
limbaba1120 <limkw1120ss@gmail.com>
2023-04-09 20:16:08 +0900

    [2주차] 임건우 과제 제출

---
+++ 건우/Week2_문자열뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = my_string.split('')
+
+    answer = answer.reverse()
+
+    answer = answer.join('')
+   
+   return console.log(answer);
+}
+
+solution("jaron");
