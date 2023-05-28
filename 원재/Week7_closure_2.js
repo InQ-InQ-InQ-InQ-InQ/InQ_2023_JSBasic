ab09fbc761541eb68c92aad64e4033ac1eb35498
woneze <00wonjae@gmail.com>
2023-05-28 16:57:44 +0900

    [7주차] 이원재 과제 제출

---
+++ 원재/Week7_closure_2.js
@@ -0,0 +1,11 @@
+function inBetween(a, b) {
+    return function(c) {
+        return c >= a && c <= b;
+    };
+}
+
+function inArray (arr) {
+    return function(a) {
+        return arr.includes(a);
+    };
+}
